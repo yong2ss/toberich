7676fe274e82e63fcfce6feb5847afcd5f89a6e0
yong2ss <korea92co@gmail.com>
2021-01-30 21:08:06 +0900

    Initial commit

---
+++ src/main/java/com/yong2ss/toberich/ToberichApplication.java
@@ -0,0 +1,13 @@
+package com.yong2ss.toberich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToberichApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToberichApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yong2ss/toberich/ToberichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yong2ss.toberich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToberichApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
