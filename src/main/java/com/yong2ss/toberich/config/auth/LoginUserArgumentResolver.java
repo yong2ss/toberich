5aa890c01bc208ba9ee002d3700f14cd71491f4a
yong2ss <korea92co@gmail.com>
2021-06-09 19:59:44 +0900

    oAuth2 부분 적용 (Google) - @LoginUser 어노테이션 추가

---
+++ src/main/java/com/yong2ss/toberich/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,34 @@
+package com.yong2ss.toberich.config.auth;
+
+import com.yong2ss.toberich.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+
+    //@LoginUser 어노테이션이 붙어 있고, 파라미터 클래스 타입이 SessionUser.class인 경우 true 반환
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    //파라미터에 전달할 객체 생성
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/com/yong2ss/toberich/web/WebController.java
+++ src/main/java/com/yong2ss/toberich/web/WebController.java
@@ -1,5 +1,6 @@
 package com.yong2ss.toberich.web;
 
+import com.yong2ss.toberich.config.auth.LoginUser;
 import com.yong2ss.toberich.config.auth.dto.SessionUser;
 import com.yong2ss.toberich.domain.posts.PostsRepository;
 import com.yong2ss.toberich.domain.user.User;
@@ -10,6 +11,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+
+//  로그인 세션 저장소 3가지 방법
+//1. 톰캣 세션을 사용 -> 세션이 내장 톰캣의 메모리에 저장되므로(WAS의 메모리), 애플리케이션 재실행시 초기화되고 2대 이상의 WAS 세션 공유 추가 설정 필요
+//2. 데이터베이스를 세션 저장소로 사용 -> 로그인 요청마다 DB IO가 발생하여 이슈
+//3. Redis같은 메모리 DB를 세션 저장소로 사용 -> 실제 서비스에선 외부 메모리 서버가 필요
+
+//보통 1번으로 여러 WAS의 세션 공유 추가 설정을 하거나, 2번을 사용
+//서비스가 굉장히 커진다면 3번 같은 AWS 레디스나 엘리스틱 캐시에 등에 금액을 지불하고 사용
 
 @RequiredArgsConstructor
 @Controller
@@ -25,10 +34,21 @@ public class WebController {
         return "main";
     }
 
-    // 메인 페이지
+    //메인 페이지(로그인) TO-BE
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "index";
+    }
+
+    /*
+    //==* 메인 페이지(로그인) AS-IS (@LoginUser 사용 전에는, 직접 세션에서 user정보 받아온다.)
     @GetMapping("/")
     public String index(Model model) {
-        //model.addAttribute("posts", postsService.findAllDesc());
         SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
@@ -37,4 +57,5 @@ public class WebController {
 
         return "index";
     }
+    */
 }
